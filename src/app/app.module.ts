eb9f19e53e35df69bcb5ee9d1917124ad2f7b4fe
mahansky <m.pro.mark@gmail.com>
2018-12-19 18:59:16 +0100

    Header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { ContentComponent } from './components/pages/content/content.component';
+import { HeaderComponent } from './components/layout/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContentComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
