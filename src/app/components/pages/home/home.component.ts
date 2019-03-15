494458f47f3afdb66301191b224f0d4d2d1e8303
mahansky <m.pro.mark@gmail.com>
2019-03-15 15:24:32 +0100

    Hide TryItOut button when REPL is disabled

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  replEnabled: boolean;
 
   constructor() { }
 
   ngOnInit() {
+    this.replEnabled = environment.replEnabled;
   }
-
 }
